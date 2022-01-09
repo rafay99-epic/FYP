6d29553def30397e4fcc60ae206da1024dbfd3b3
Abdul Rafay <99marafay@gmail.com>
2022-01-09 18:28:33 +0500

    Android Application for user

---
+++ User-Android-Application/API/includes/Constants.php
@@ -0,0 +1,7 @@
+<?php 
+	define('DB_NAME','parking_data');
+	define('DB_USER','root');
+	define('DB_PASSWORD','');
+	define('DB_HOST','localhost');
+
+	
---
+++ User-Android-Application/Android Application/app/src/main/java/com/example/userandroidapplication/Constants.java
@@ -0,0 +1,5 @@
+package com.example.userandroidapplication;
+
+public class Constants {
+    private static final String ROOT_URL="http://192.168.100.87/FYP/User-Android-Application/API/v1/";
+}
