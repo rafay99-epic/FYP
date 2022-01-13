9db48e9d67ddc2fd3c1f3c3e9ad57614fbba49de
Abdul Rafay <99marafay@gmail.com>
2022-01-13 23:47:48 +0500

    Audrino Code

---
+++ Audrino Code/sketch_jan13c/sketch_jan13c.ino
@@ -0,0 +1,41 @@
+void setup() {
+  pinMode(13,INPUT);
+  pinMode(3,INPUT);
+  Serial.begin(9600);
+  pinMode(9,OUTPUT);
+
+
+  pinMode(13,INPUT);
+  pinMode(5,INPUT);
+  Serial.begin(9600);
+  pinMode(9,OUTPUT);
+}
+void loop() {
+  
+Serial.print("IRSensorip1  ");
+Serial.println(digitalRead(3));
+if(digitalRead(3)==0)
+{
+  digitalWrite(13,HIGH);
+   delay(10);
+  }
+ else{
+    digitalWrite(13,LOW);
+     delay(10);
+    }
+
+Serial.print("IRSensorip2  ");
+Serial.println(digitalRead(5));
+if(digitalRead(5)==0)
+{
+  digitalWrite(13,HIGH);
+   delay(10);
+  }
+ else{
+    digitalWrite(13,LOW);
+     delay(10);
+    }
+
+
+ 
+}
