e09fc4b56534b907afea69247f161443799baa3e
KHILJI <sameermalik0478@gmail.com>
2022-02-22 14:44:12 +0500

    Update Live View Status From Sensors
    Table Created For Real Time status Update from IR Sensors.

--- Manager/view-status.php
+++ Manager/view-status.php
@@ -1,24 +1,48 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>View Live location</title>
-	<link rel="stylesheet" type="text/css" href="styles.css">
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js'></script>
-    <script>
+    <title>View Live location</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="./socket.io.js"></script>
+    <script src="./SensorOnOff.js"></script>
 
-    var socket = io();
-
-    socket.on('data', function(data) {
-        console.log(data);
-        document.getElementById('sample').style.opacity = data+"%"; 
-    });
-
-    </script>
 </head>
+
 <body>
-     <form action="" method="">
-     	<h2>Live Locations of Parking Space</h2>
-       
-     </form>
+    <form action="" method="">
+        <h2>Live Locations of Parking Space</h2>
+
+        <table style="width:100%">
+            <tr>
+                <th>Sensors</th>
+                <th>On/Off</th>
+            </tr>
+            <tr>
+                <td id="sensor1"></td>
+                <td id="sensor1On/Off"></td>
+
+            </tr>
+            <tr>
+                <td id="sensor2"></td>
+                <td id="sensor2On/Off"></td>
+            </tr>
+            <tr>
+                <td id="sensor3"></td>
+                <td id="sensor3On/Off"></td>
+            </tr>
+            <tr>
+                <td id="sensor4"></td>
+                <td id="sensor4On/Off"></td>
+            </tr>
+            <tr>
+                <td id="sensor5"></td>
+                <td id="sensor5On/Off"></td>
+            </tr>
+        </table>
+
+    </form>
+
 </body>
+
 </html>
